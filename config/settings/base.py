f15993d22a46679bc33ff6744021737916a40623
vigneshhari <vichuhari100@gmail.com>
2026-08-18 21:55:33 +0530

    Fix linting and configs

--- config/settings/base.py
+++ config/settings/base.py
@@ -1,5 +1,6 @@
 # ruff: noqa: ERA001, E501
 """Base settings to build other settings files upon."""
+
 import os
 import ssl
 from pathlib import Path
@@ -63,7 +64,6 @@ else:
 
 DATABASES["default"]["ATOMIC_REQUESTS"] = True
 # https://docs.djangoproject.com/en/stable/ref/settings/#std:setting-DEFAULT_AUTO_FIELD
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # URLS
 # ------------------------------------------------------------------------------
@@ -88,7 +88,7 @@ DJANGO_APPS = [
 
 TAILWIND_APP_NAME = "theme"
 
-NPM_BIN_PATH=env(
+NPM_BIN_PATH = env(
     "NPM_BIN_PATH",
     default="/usr/bin/node",
 )
@@ -102,10 +102,8 @@ THIRD_PARTY_APPS = [
     "allauth.socialaccount",
     "django_celery_beat",
     "corsheaders",
-        "tailwind",
-        "theme"
-
-
+    "tailwind",
+    "theme",
 ]
 
 LOCAL_APPS = [
