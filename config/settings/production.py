ab659574112f1ef8f440fc96be62d0ff276f0d33
Bodhish Thomas <bodhish@gmail.com>
2026-08-19 17:23:35 +0530

    Add resend

--- config/settings/production.py
+++ config/settings/production.py
@@ -138,12 +138,11 @@ ADMIN_URL = env("DJANGO_ADMIN_URL")
 INSTALLED_APPS += ["anymail"]
 # https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
 # https://anymail.readthedocs.io/en/stable/installation/#anymail-settings-reference
-# https://anymail.readthedocs.io/en/stable/esps/mailgun/
-EMAIL_BACKEND = "anymail.backends.mailgun.EmailBackend"
+# https://anymail.readthedocs.io/en/stable/esps/resend/
+EMAIL_BACKEND = "anymail.backends.resend.EmailBackend"
 ANYMAIL = {
-    "MAILGUN_API_KEY": env("MAILGUN_API_KEY"),
-    "MAILGUN_SENDER_DOMAIN": env("MAILGUN_DOMAIN"),
-    "MAILGUN_API_URL": env("MAILGUN_API_URL", default="https://api.mailgun.net/v3"),
+    "RESEND_API_KEY": env("RESEND_API_KEY"),
+    "RESEND_API_URL": env("RESEND_API_URL", default="https://api.resend.com/"),
 }
 
 # django-compressor
