9348c727f2743ba5c7610c6269ff4951e78d6411
Bodhish Thomas <bodhish@gmail.com>
2026-08-19 17:37:53 +0530

    Change the email domain

--- config/settings/production.py
+++ config/settings/production.py
@@ -116,7 +116,7 @@ MEDIA_URL = f"https://{aws_s3_domain}/media/"
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-from-email
 DEFAULT_FROM_EMAIL = env(
     "DJANGO_DEFAULT_FROM_EMAIL",
-    default="OHC Experience <noreply@ohc.network>",
+    default="OHC Experience <noreply@experience.ohc.network>",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#server-email
 SERVER_EMAIL = env("DJANGO_SERVER_EMAIL", default=DEFAULT_FROM_EMAIL)
