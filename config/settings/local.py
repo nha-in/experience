84713c1c81d9b2faa7f93dfd1c2c4d3ce3d7dfcd
Bodhish Thomas <bodhish@gmail.com>
2026-09-09 18:55:48 +0530

    Revert "Allow native development to send email through local Mailtrap"
    This reverts commit a3f3e34a37ccb528c87a00949e0464612d8ded0f.

--- config/settings/local.py
+++ config/settings/local.py
@@ -113,13 +113,10 @@ STORAGES = {
 # Your stuff...
 # ------------------------------------------------------------------------------
 
-# Shell development defaults to disk uploads and console mail.
+# Shell development uses disk uploads and console mail without Docker services.
 if env.bool("DJANGO_USE_LOCAL_MEDIA", default=env("USE_DOCKER", default="no") != "yes"):
     STORAGES["default"] = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     }
 if env("USE_DOCKER", default="no") != "yes":
-    EMAIL_BACKEND = env(
-        "DJANGO_EMAIL_BACKEND",
-        default="django.core.mail.backends.console.EmailBackend",
-    )
+    EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
